da6da898ae1b52d3cc94d1037ff1d3179d5d967a
Peyton <peytonstrahan@outlook.com>
2024-11-13 17:32:46 -0600

    Home button, styling, and clickable usernames

--- index.js
+++ index.js
@@ -2,21 +2,24 @@
 $(document).ready(() => {
   const $body = $('body');
   $body.html(''); // clear the body
+  const $siteTitle = $('<h1>Garble</h1>'); // create the website's big display name/title
+  $body.append($siteTitle);
   const $refreshButton = $('<button>Refresh Tweets</button>'); //.width('100').height('40'); // create a refresh button to allow for manual refresh of tweets
   $body.append($refreshButton);
-  const $tweetsContainer = $('<div></div>'); //.attr('id', 'tweets-container'); // create a container div to separate the tweets from the rest of the body
+  const $homeButton = $('<button>Home (show all tweets)</button>'); //.width('100').height('40'); // create a home button to allow to switch back to the home stream (includes all tweets) and to refresh
+  $body.append($homeButton);
+  const $tweetsContainer = $('<div></div>').css('background-color', 'green'); //.attr('id', 'tweets-container'); // create a container div to separate the tweets from the rest of the body
   $body.append($tweetsContainer);
+  let streamSource = streams.home; // create a stream source variable that is set to the array of tweets in home by default (this array includes all tweets by all users)
 
-  const refresh = function() { // create a function that will refresh the tweets on the page whenever it is called
+  const refresh = function(inputStreamSource = streams.home) { // create a function that takes in an array of tweets (is home by default) and will refresh the tweets on the page to be the tweets from the given array whenever it is called 
     $tweetsContainer.html(''); // clear the tweets container div
-    const $tweets = streams.home.map((tweet) => { // create an array of tweet divs
-      const $tweet = $('<div></div>'); // create the tweet div
-      // const username = $(`<div>@${tweet.user}:</div>`); // create the username div
-      // const message = $(`<div> ${tweet.message}</div>`); // create the message div
-      
-      const text = `@${tweet.user}: ${tweet.message}`; // put the current tweet's author (user) and message into the div
-  
-      $tweet.text(text);
+    const $tweets = inputStreamSource.map((tweet) => { // create an array of tweet divs
+      const $tweet = $('<div></div>').addClass('tweet'); // create the tweet div
+      const $username = $(`<div>@${tweet.user}:</div>`).addClass('username').attr('id', tweet.user); // create the username div
+      const $message = $(`<div> ${tweet.message}</div>`).addClass('message'); // create the message div
+
+      $tweet.append([$username, $message]); // put the current tweet's author (username) div and message div into the tweet div
 
       const $timestamp = $('<div></div>').addClass('timestamp'); // create a div for the timestamp
       const formTimestamp = moment(tweet.created_at).format('MMMM DD, YYYY LT'); // use moment to create and format a timestamp (date and time) derived from the current tweet object's created_at property
@@ -27,10 +30,39 @@ $(document).ready(() => {
       return $tweet;
     });
     $tweetsContainer.append($tweets.reverse()); // append the array of tweet divs to the tweets container div, making sure to reverse it so that the newest tweets show up first
+
+    $('.username').on('click', function() { // Changes the current stream source to be the one associated with the clicked-on username. This also refreshes the stream's tweets.
+      //const userTimeline = streams.home.filter((tweet) => tweet.user === $(this).attr('id'));
+      streamSource = streams.users[$(this).attr('id')];
+      refresh(streamSource);
+    });
+
+    // STYLING (for everything contained inside of the tweets container div)
+    $('.username').hover( // usernames change color when hovered over
+      function() {
+        $(this).css("color", "blue");
+      },
+      function() {
+        $(this).css("color", "black");
+      }
+    );
+
+    $('.tweet').css('margin', '10px').css('background-color', 'orange');
+    $('.username').css('float', 'left');
+    $('.message').css('white-space', 'pre-wrap');
   };
 
   refresh(); // initial use of refresh function to prevent empty page on start up
-  $refreshButton.on('click', function() { // give the refresh button the ability to refresh the tweets when clicked
+
+  $refreshButton.on('click', function() { // give the refresh button the ability to refresh the current stream's tweets when clicked
+    refresh(streamSource);
+  });
+
+  $homeButton.on('click', function() { // give the home button the ability to reset the current stream source to home and to refresh the stream's tweets
+    streamSource = streams.home;
     refresh();
   });
+
+  // STYLING (general)
+
 });
