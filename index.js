9ee04e581606a24eca91b7410f9603294e509192
Peyton <peytonstrahan@outlook.com>
2024-11-14 17:18:36 -0600

    Made it pretty

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ $(document).ready(() => {
   $body.html(''); // clear the body
   const $siteTitle = $('<h1>Garble</h1>'); // create the website's big display name/title
   $body.append($siteTitle);
-  const $refreshButton = $('<button>Refresh Tweets</button>'); //.width('100').height('40'); // create a refresh button to allow for manual refresh of tweets
+  const $siteLogo = $('<image src="https://img00.deviantart.net/a76c/i/2011/030/8/b/wh40k_adeptus_mechanicus_icon_by_hedge_ki_sama-d38exra.png"><br>'); // create the website's logo image
+  $body.append($siteLogo);
+  const $refreshButton = $('<button><div class="buttonChild">Refresh Messages</div></button>'); // create a refresh button to allow for manual refresh of tweets
   $body.append($refreshButton);
-  const $homeButton = $('<button>Home (show all tweets)</button>'); //.width('100').height('40'); // create a home button to allow to switch back to the home stream (includes all tweets) and to refresh
+  const $homeButton = $('<button><div class="buttonChild">Home (show all messages)</div></button>'); // create a home button to allow to switch back to the home stream (includes all tweets) and to refresh
   $body.append($homeButton);
-  const $messageInputForm = $('<form></form>').css('background-color', 'darkgray'); // create a form that allows users to post messages that will show up in the stream
+  const $messageInputForm = $('<form></form>'); // create a form that allows users to post messages that will show up in the stream
   $body.append($messageInputForm);
-  const $tweetsContainer = $('<div></div>').css('background-color', 'green'); //.attr('id', 'tweets-container'); // create a container div to separate the tweets from the rest of the body
+  const $tweetsContainer = $('<div></div>'); // create a container div to separate the tweets from the rest of the body
   $body.append($tweetsContainer);
   let streamSource = streams.home; // create a stream source variable that is set to the array of tweets in home by default (this array includes all tweets by all users)
 
@@ -34,7 +36,6 @@ $(document).ready(() => {
     $tweetsContainer.append($tweets.reverse()); // append the array of tweet divs to the tweets container div, making sure to reverse it so that the newest tweets show up first
 
     $('.username').on('click', function() { // Changes the current stream source to be the one associated with the clicked-on username. This also refreshes the stream's tweets.
-      //const userTimeline = streams.home.filter((tweet) => tweet.user === $(this).attr('id'));
       streamSource = streams.users[$(this).attr('id')];
       refresh(streamSource);
     });
@@ -43,13 +44,15 @@ $(document).ready(() => {
     $('.username').hover( // usernames change color when hovered over
       function() {
         $(this).css("color", "blue");
+        $(this).css("text-decoration", "underline");
       },
       function() {
         $(this).css("color", "black");
+        $(this).css("text-decoration", "unset");
       }
     );
 
-    $('.tweet').css('margin', '10px').css('background-color', 'orange');
+    $('.tweet').css('margin', '1.5%').css('background-image', 'linear-gradient(white, darkgray)').css('border-style', 'solid').css('border-width', '5px').css('border-color', 'orange');
     $('.username').css('float', 'left');
     $('.message').css('white-space', 'pre-wrap');
   };
@@ -96,5 +99,13 @@ $(document).ready(() => {
   });
 
   // STYLING (general)
-
+  $body.css('background-image', 'url(https://media.giphy.com/media/YFI1WOcZaYKZsbVWhF/giphy.gif)').css('background-size', '100%').css('font-size', '300%');
+  $siteTitle.css('color', 'red').css('padding-left', '115px').css('padding-top', '2%').css('font-family', 'fantasy');
+  $messageInputForm.css('background-image', 'linear-gradient(darkgray, grey, orange, red)').css('margin', 'auto').css('padding-bottom', '10px').css('width', '75%').css('border-radius', '15px');
+  $('#inputName').css('margin', 'auto').css('display', 'grid').css('width', '90%').css('font-size', '50px').css('font-family', 'serif');
+  $('#inputMessage').css('margin', 'auto').css('display', 'grid').css('width', '90%').css('font-size', '50px').css('font-family', 'serif');
+  $('#submit').css('background-image', 'radial-gradient(cyan, blue, darkgray, black)').css('margin', 'auto').css('display', 'grid').css('font-size', '150%').css('font-family', 'serif').css('border-radius', '100%');
+  $tweetsContainer.css('background-color', 'green').css('margin', 'auto').css('margin-top', '10px').css('display', 'grid').css('width', '50%').css('border-radius', '30px');
+  $('button').css('background-image', 'repeating-linear-gradient(45deg, rgb(150, 150, 150) 0px, black 30px, yellow 30px, maroon 60px)').css('margin-left', '20%').css('margin-bottom', '10px').width('20%').height('100px').css('border-style', 'solid').css('border-width', '10px').css('border-color', 'silver');
+  $('.buttonChild').css('background-color', 'gray').css('font-size', '40px').css('font-family', 'serif').css('font-weight', 'bold').css('border-radius', '15px');
 });
